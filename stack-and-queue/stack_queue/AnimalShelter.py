a7a00e0dbe61d1045fa986054f5bbae181cf9c13
asfantala <22037049@student.ltuc.com>
2023-05-15 09:13:19 +0000

    doc string

--- stack-and-queue/stack_queue/AnimalShelter.py
+++ stack-and-queue/stack_queue/AnimalShelter.py
@@ -19,6 +19,13 @@ class Cat(Animal):
         super().__init__("cat", name)
 
 class AnimalShelter():
+
+  '''
+  When dequeuing,iterate through the queue until you find an animal with the preferred species (pref). If found, that animal is dequeued and returned.
+If the preferred species is not found, until the main animals queue is empty.
+.
+  
+  '''
   def __init__(self):
     self.cats, self.dogs = [], []
   
@@ -40,7 +47,8 @@ class AnimalShelter():
     return cat
     
   def dequeueDog(self):
-    if len(self.dogs) == 0: return None
+    if len(self.dogs) == 0: 
+       return None
     dog = self.dogs[0]
     self.dogs = self.dogs[1:]
     return dog
