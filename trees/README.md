22883d73c602c2aeacbdcc626a553d7ccb585972
asfantala <22037049@student.ltuc.com>
2023-05-20 14:04:11 +0000

    tests and whiteboard added

--- trees/README.md
+++ trees/README.md
@@ -3,6 +3,7 @@
 
 ## Whiteboard Process
 <!-- Embedded whiteboard image -->
+![](Untitled(12).jpg)
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
@@ -11,3 +12,84 @@
 - The Big O space complexity of a BST search would be O(1). During a search, we are not allocating any additional space.
 ## Solution
 <!-- Show how to run your code, and examples of it in action -->
+
+```
+
+    def __init__(self):
+        self.root = None
+
+   
+    def pre_order(self, root):
+     result = []  # Create an empty list to store the values
+
+     if root is not None:
+        result.append(root.value)  # Append the value to the result list
+        result.extend(self.pre_order(root.left))  # Recursively traverse the left subtree
+        result.extend(self.pre_order(root.right))  # Recursively traverse the right subtree
+
+     return result
+
+
+    def in_order(self, root):
+     result = []  # Create an empty list to store the values
+
+     if root is not None:
+        result.extend(self.in_order(root.left))  # Recursively traverse the left subtree
+        result.append(root.value)  # Append the value to the result list
+        result.extend(self.in_order(root.right))  # Recursively traverse the right subtree
+
+     return result
+
+
+    def post_order(self, root):
+     result = []  # Create an empty list to store the values
+
+     if root is not None:
+        result.extend(self.post_order(root.left))  # Recursively traverse the left subtree
+        result.extend(self.post_order(root.right))  # Recursively traverse the right subtree
+        result.append(root.value)  # Append the value to the result list
+
+     return result
+
+
+
+class BinarySearch(Binary_tree):
+   
+    def add(self, value):
+
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        current = self.root
+        while current:
+            if value < current.value:
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+                else:
+                    current = current.left
+            elif value > current.value:
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+                else:
+                    current = current.right
+            else:
+                return
+    def contains(self, value):
+
+        current = self.root
+        while current:
+            if current.value == value:
+                return True
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+
+        return False
+    
+
+
+```
--- trees/trees/Binary_tree.py
+++ trees/trees/Binary_tree.py
@@ -1,47 +1,97 @@
 from trees.Node import Node
 
 class Binary_tree:
+
+    '''
+    Create a Binary Tree class
+Define a method for each of the depth first traversals:
+pre order
+in order
+post order
+Each depth first traversal method should return an array of values, ordered appropriately.
+    '''
     def __init__(self):
         self.root = None
 
-    def pre_order(self,root):
-        if root is not None:
-            print(root.value)
-        if root.left is not None:
-            self.pre_order(root.left)
-        if root.right is not None:
-            self.pre_order(root.right)
-    # Inorder traversal
-# Left -> Root -> Right
-          
+   
+    def pre_order(self, root):
+     result = []  # Create an empty list to store the values
 
-    def in_order(self,root):
-        if root.left is not None:
-            self.in_order(root.left)
-        if root is not None:
-            print(root.value)
-        if root.right is not None:
-            self.in_order(root.right)
+     if root is not None:
+        result.append(root.value)  # Append the value to the result list
+        result.extend(self.pre_order(root.left))  # Recursively traverse the left subtree
+        result.extend(self.pre_order(root.right))  # Recursively traverse the right subtree
+
+     return result
 
 
+    def in_order(self, root):
+     result = []  # Create an empty list to store the values
 
-# pre_order(tree1.root)
-# tree1 = Tree()
+     if root is not None:
+        result.extend(self.in_order(root.left))  # Recursively traverse the left subtree
+        result.append(root.value)  # Append the value to the result list
+        result.extend(self.in_order(root.right))  # Recursively traverse the right subtree
 
-# node1 = Node("A")
-# tree1.root = node1
+     return result
 
-# node2 = Node("B")
-# tree1.root.left = node2
 
-# node3 = Node("C")
-# tree1.root.right = node3
+    def post_order(self, root):
+     result = []  # Create an empty list to store the values
 
-# node4 = Node("D")
-# tree1.root.left.left = node4
+     if root is not None:
+        result.extend(self.post_order(root.left))  # Recursively traverse the left subtree
+        result.extend(self.post_order(root.right))  # Recursively traverse the right subtree
+        result.append(root.value)  # Append the value to the result list
 
-# node5 = Node("E")
-# tree1.root.left.right = node5
+     return result
 
-# node6 = Node("F")
-# tree1.root.right.left = node6
+
+
+class BinarySearch(Binary_tree):
+    """
+    Binary Search Tree class
+    This class should be a sub-class (or your languages equivalent) of the Binary Tree Class, with the following additional methods:
+
+    Add -Arguments: value ,Return: nothing, Adds a new node with that value in the correct location in the binary search tree.
+
+    Contains -Argument: value ,Returns: boolean indicating whether or not the value is in the tree at least once
+    
+    """
+    def add(self, value):
+
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        current = self.root
+        while current:
+            if value < current.value:
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+                else:
+                    current = current.left
+            elif value > current.value:
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+                else:
+                    current = current.right
+            else:
+                return
+    def contains(self, value):
+
+        current = self.root
+        while current:
+            if current.value == value:
+                return True
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+
+        return False
+    
+
+
