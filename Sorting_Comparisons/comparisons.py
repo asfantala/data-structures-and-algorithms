8e581214f5d9b7ee5d394fe01b866e0d99273532
asfantala <22037049@student.ltuc.com>
2023-06-19 21:20:29 +0000

    added

--- Sorting_Comparisons/comparisons.py
+++ Sorting_Comparisons/comparisons.py
@@ -1,16 +1,18 @@
-def sort_movies_by_year(movies):
-    sorted_movies = sorted(movies, key=lambda movie: movie['year'], reverse=True)
-    return sorted_movies
+def sort_by_name(movies):
+    return sorted(movies, key=lambda movie: format_movie(movie["title"]))
 
-def sort_movies_by_title(movies):
-    def remove_leading_articles(title):
-        articles = ['The ', 'An ', 'A ']
-        for article in articles:
-            if title.startswith(article):
-                return title[len(article):]
-        return title
 
-    sorted_movies = sorted(movies, key=lambda movie: remove_leading_articles(movie['title'].lower()))
-    return sorted_movies
+def sort_by_year(movies):
+    return sorted(movies, key=lambda movie: movie["year"])
 
+
+def format_movie(title: str):
+    lower_title = title.lower()
+    if lower_title.startswith("the "):
+        return lower_title[4:]
+    if lower_title.startswith("a "):
+        return lower_title[2:]
+    if lower_title.startswith("an "):
+        return lower_title[3:]
+    return lower_title
 
